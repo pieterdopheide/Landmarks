c99dca18dadfa64e01978226fe843b84a62b4e65
Pieter Dopheide <pieter.dopheide@oclc.org>
2024-01-24 11:02:45 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Dopheide,Pieter on 24/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
