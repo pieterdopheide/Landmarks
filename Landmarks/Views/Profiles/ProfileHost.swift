09232bc30191b50baef2313c68d7ff1962a5842d
Pieter Dopheide <pjmdopheide@gmail.com>
2024-02-02 13:35:12 +0100

    Display a user profile

---
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by Dopheide,Pieter on 02/02/2024.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ProfileHost()
+}
